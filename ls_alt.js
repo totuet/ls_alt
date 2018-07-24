8001937f41d821aea54016bdb5406a4137ea4ffe
Tom van den Berg <Tomvdb@Mac-mini.local>
2018-07-25 00:18:26 +0200

    first commit

---
+++ ls_alt.js
@@ -0,0 +1,22 @@
+/*
+NodeJS implementation of 'ls' (list files in Unix et al)
+
+*/
+const fs = require('fs');
+
+fs.readdir('./', (err, files) => {
+    if (err) {
+        console.log("error");
+    };
+    // to find the length of an object you call .keys() on it, this
+    // creates an array with all objects names
+    var lengthArray = Object.keys(files).length;
+
+    for (i = 0; i < lengthArray; i++) {
+        //statSync shows information about a file
+        var t = fs.statSync(`./${files[i]}`);
+        var tm = t.atime.toString().split('G');
+
+        console.log(`${files[i]} \t ${t.size}b \t ${tm[0]}`);
+         }
+} );
